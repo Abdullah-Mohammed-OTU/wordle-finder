722ef6377f9c781ecf0e236e09ef5b81297ac894
Abdullah-Mohammed-OTU <abdullah.mohammed@ontariotechu.net>
2022-03-31 15:29:26 -0400

    updated version

--- wordlefindrbot.py
+++ wordlefindrbot.py
@@ -1,7 +1,18 @@
 
-def main():
-    # array = [] 
-    with open("Wordle Answers.txt",'w') as file: 
-        lines = file.readlines()
+def get_words():
+    array = [] 
+    with open('Wordle Answers.txt','r') as file: 
+        for line in file: 
+            data = line.split()
+            array.append(data[3])
+    return array 
 
-main()
+def main(): 
+    how_many_letters = input("Enter how many letters you know: ")
+    position = input("Enter the location of ")
+    if how_many_letters >= 1 and how_many_letters <=4: 
+        for i in range(0, how_many_letters): 
+            input
+
+if __name__ == "__main__":
+   main()
